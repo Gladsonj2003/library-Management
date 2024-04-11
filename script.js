549ef3f4a7ba38046c26912a89b4f654a3d93d2d
Gladson Joel Raja J <joelraja2016@gmail.com>
2024-04-11 12:10:45 +0530

    created html,css,javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+var popupoverelay=document.querySelector(".popup-overelay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverelay.style.display="block"
+    popupbox.style.display="block"
+})
+
+var cancelbutton=document.getElementById("cancel-popup")
+cancelbutton.addEventListener("click",function(){
+    event.preventDefault()
+    popupoverelay.style.display="none"
+    popupbox.style.display="none"
+})
+
+var container=document.querySelector(".container")
+var addbook=document.getElementById("add-book")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+    <h5>${bookauthorinput.value}</h5>
+    <p>${bookdescriptioninput.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    popupoverelay.style.display="none"
+    popupbox.style.display="none"
+})
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
